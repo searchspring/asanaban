38894be839f5dc55bd48cf04d8067dc8a7641f6b
codeallthethingz <howdoicontactwill@gmail.com>
2021-01-24 15:34:39 -0800

    moved to terser

--- brunch-config.js
+++ brunch-config.js
@@ -17,6 +17,14 @@ exports.plugins = {
       ["transform-react-jsx", { pragma: 'm' }]
     ]
   },
+  // terser: {
+  //   mangle: false,
+  //   compress: {
+  //     global_defs: {
+  //       DEBUG: false,
+  //     },
+  //   },
+  // },
   postcss: {
     processors: [
       require('tailwindcss')
--- deployProduction.sh
+++ deployProduction.sh
@@ -1,4 +1,1 @@
-go get 
-go run create-dist.go || exit 1
 vercel --prod && vercel rm asanaban --safe --yes
-rm -rf dist
