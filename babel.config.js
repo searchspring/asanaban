b4381eea3bac161778282ac11ae019bb10f5c3f2
codeallthethingz <howdoicontactwill@gmail.com>
2021-10-27 13:17:49 -0700

    vue3 scaffold

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
-  plugins: ["@babel/plugin-transform-react-jsx"]
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
